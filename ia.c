31d06eedf7c88998b1ec535a1e904e7cd1177a0a
DrZomgwtfbbq <keyserkiller@yahoo.fr>
2013-12-25 17:58:16 +0100

    struct finale

--- ia.c
+++ ia.c
@@ -1,24 +1,24 @@
 
 typedef struct
 {
-    int idBoard[10][10];	// Tableau du jeu de 10 cases par 10. dim1=ligne dim2=colonne
-    playerBox player[40];	// Tableau contenant des playerBox renseignant les infos sur les pieces alliés.
-    unsigned int blueOut[11];	// Tableau de comptage des pièces bleues éliminées (indexées par la valeur de EPiece : EPbomb=0, ..., EPmarshal=10
+	int idBoard[10][10];	// Tableau du jeu de 10 cases par 10. dim1=ligne dim2=colonne. Chaque pion est renseigne par un id, les lacs par -1 et les cases vides par 0
+	playerBox player[40];	// Tableau contenant des playerBox renseignant les infos sur les pieces alliés.
+	ennemyBox ennemy[40];	// Tableau contenant des playerBox renseignant les infos sur les pieces ennemies.
 } SIaState;
 
 typedef struct
 {
-    int ligne; //ligne dans laquelle se trouve la piece
-    int colonne;//colonne dans laquelle se trouve la piece
-    int value; //valeur de la piece
-    int booleanKnown; //Boolean vallant vrai si la valeur de cette piece a deja ete rEvele a l'ennemi
-} playerBox;
+	int ligne; //ligne dans laquelle se trouve la piece
+	int colonne;//colonne dans laquelle se trouve la piece
+	int value; //valeur de la piece
+	int booleanKnown; //Boolean vallant vrai si la valeur de cette piece a deja ete rEvele a l'ennemi
+		} playerBox;
 
 typedef struct
 {
-    int ligne; //ligne dans laquelle se trouve la piece
-    int colonne;//colonne dans laquelle se trouve la piece
-    int value; //valeur supposee de la piece
-    //possiblement value[12] pourcentage de proba pour chaque type de piece
-    int booleanKnown; //Boolean vallant vrai si la valeur de cette piece a deja ete rEvele a l'ennemi
-} ennemyBox;
+	int ligne; //ligne dans laquelle se trouve la piece
+	int colonne;//colonne dans laquelle se trouve la piece
+	int value; //valeur supposee de la piece
+	//possiblement value[12] pourcentage de proba pour chaque type de piece
+	int booleanHasMoved; //Boolean vallant vrai si la piece s'est déja déplacé (potentiellement supprimable: probabilité drapeau/bombe non nulle par défaut, passé à 0 des premier déplacement)
+		} ennemyBox;
